b9867baa8454ef8750567924a94a700a3ecbffa1
Tom Quinlan <tquinlan1992@gmail.com>
2019-03-02 14:23:08 -0500

    set up code component

--- src/components/Code/index.tsx
+++ src/components/Code/index.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
-import { Props, Actions, mapStateToProps, mapDispatchToProps } from './mapProps';
-import { connect, Omit } from "react-redux";
-import { AppState } from '@headless/store';
+import MonacoEditor from 'react-monaco-editor';
+import { Button } from '@material-ui/core';
 import { getRemoteDb } from "@headless/database/pouch";
-import { SelectCodeConnect } from '../SelectCode';
-import { Code } from '@components/Code';
 
-interface ActionsNoThunk extends Omit<Actions, 'loadCode' | 'saveCode'> {
+interface Props {
+    code: string;
     loadCode: () => void;
     saveCode: (newCode: string) => void;
 }
 
-export class EditCode extends React.Component<Props & ActionsNoThunk> {
+export class Code extends React.Component<Props> {
 
     state = {
         tickets: [],
@@ -28,7 +26,7 @@ export class EditCode extends React.Component<Props & ActionsNoThunk> {
     }
 
     consoleLog(text: string) {
-        this.setState({ console: text });
+        this.setState({console: text});
     }
 
     async run() {
@@ -42,36 +40,39 @@ export class EditCode extends React.Component<Props & ActionsNoThunk> {
         try {
             data = await evalCode.data();
             const evalComponents = evalCode.components(data);
-            this.setState({ evalComponents });
-        } catch (e) {
+            this.setState({evalComponents});
+        } catch(e) {
 
         }
         // eval(`(${this.props.code})('created with eval function')`);
     }
 
-    getEvalComponent({ type, value }: { type: string; value: any; }) {
-        switch (type) {
+    getEvalComponent({type, value}: {type: string; value: any;}) {
+        switch(type) {
             case 'label':
                 return <h1>{value}</h1>
-            default:
+            default: 
                 return null;
         }
     }
 
     render() {
-        const codeProps = {
-            code: this.props.code,
-            saveCode: this.props.saveCode,
-            loadCode: this.props.loadCode
-        }
         return (
             <React.Fragment>
-                <SelectCodeConnect />
-                <Code {...codeProps} />
+            <MonacoEditor
+                width="800"
+                height="600"
+                language="javascript"
+                theme="vs-dark"
+                value={this.props.code}
+                onChange={this.onCodeChange.bind(this)}
+            />
+            <Button onClick={this.run.bind(this)}>Run</Button>
+            <textarea value={this.state.console} />
+            {this.state.evalComponents.map(evalComponent => {
+                return this.getEvalComponent(evalComponent);
+            })}
             </React.Fragment>
         );
     }
 }
-
-export const EditCodeConnected = connect<Props, Actions, void, AppState>(mapStateToProps, mapDispatchToProps)(EditCode);
-
